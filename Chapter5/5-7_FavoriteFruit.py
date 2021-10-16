7cc45854ccec4306c5f1c66d47e9e459991e127c
Ralph <ralphdelap27@gmail.com>
2021-10-15 22:23:41 -0700

    More stuff

---
+++ Chapter5/5-7_FavoriteFruit.py
@@ -0,0 +1,17 @@
+# Make a list of favorite fruits
+
+favorite_fruits = ['apples', 'strawberries', 'oranges', 'bananas', 'grapes']
+
+# Write five if statements. Each should check whether a certain kind of fruit is in the list
+# If the fruit is in the list, the if block should print a statement, such as "You really like bananas!"
+
+if 'apples' in favorite_fruits:
+    print("You really like apples!")
+if 'pineapples' in favorite_fruits:
+    print("You really like pineapples!")
+if 'grapes' in favorite_fruits:
+    print("You really like grapes!")
+if 'kiwis' in favorite_fruits:
+    print("You really like kiwis!")
+if 'strawberries' in favorite_fruits:
+    print("You really like strawberries!")
