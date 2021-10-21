b58f218017353ffe578da51324cea16118aefef2
Ralph <ralphdelap27@gmail.com>
2021-10-20 22:14:39 -0700

    More stuff

--- Chapter5/5-10_CheckingUsernames.py
+++ Chapter5/5-10_CheckingUsernames.py
@@ -9,3 +9,13 @@ new_users = ['jontron', 'codyko', 'noelmiller', 'dannygonzalez', 'bermpeak']
 # Loop thru new_users list to see if each new username has already been used.
 # If it has, print a message that the person will need to enter a new username
 # If a username has not been used, print a message saying that the username is available
+
+# make sure its case insensitive so "John" and "JOHN" can't be two different usernames
+current_users = [current_user.lower() for current_user in current_users]
+new_users = [new_user.lower() for new_user in new_users]
+
+for new_user in new_users:
+    if new_user in current_users:
+        print("Sorry that username is taken, enter a new username")
+    else:
+        print(f"Welcome {new_user}!")
