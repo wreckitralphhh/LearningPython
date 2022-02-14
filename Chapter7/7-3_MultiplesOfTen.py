4eeaf0974a1d5bf1f4033548f4a8a3d4c0004888
Ralph <ralphdelap27@gmail.com>
2022-02-13 22:09:39 -0800

    Exercise 7-3

--- Chapter7/7-3_MultiplesOfTen.py
+++ Chapter7/7-3_MultiplesOfTen.py
@@ -1,1 +1,9 @@
 # ToDo: Ask the user for a number, then report whether the number is a multiple of 10 or not
+
+number = input("Enter a number, and I'll tell you if it's divisible by 10: ")
+number = int(number)
+
+if number % 10 == 0:
+    print(f"\nThe number {number} is divisible by 10.")
+else:
+    print(f"\nThe number {number} isn't divisible by 10.")
